805837e5413a4f81a32676216d651fcd6ec439e9
JoaoMiguelCosta <joaoxxmiguel@hotmail.com>
2026-06-21 23:21:29 +0100

    perf(sports): split page bundles

--- src/brands/sports/routes.jsx
+++ src/brands/sports/routes.jsx
@@ -5,9 +5,7 @@ const SportsShell = lazy(() => import("./layouts/SportsShell.jsx"));
 
 const HomePageSports = lazy(() => import("./pages/Home/index.jsx"));
 
-const TrainingCampsPage = lazy(() =>
-  import("./pages/TrainingCamps/index.jsx"),
-);
+const TrainingCampsPage = lazy(() => import("./pages/TrainingCamps/index.jsx"));
 
 const AcademiesPage = lazy(() => import("./pages/Academies/index.jsx"));
 
@@ -15,40 +13,32 @@ const EventsPage = lazy(() => import("./pages/Events/index.jsx"));
 
 const AthletesPage = lazy(() => import("./pages/Athletes/index.jsx"));
 
-const TestimonialsPage = lazy(() =>
-  import("./pages/Testimonials/index.jsx"),
-);
+const TestimonialsPage = lazy(() => import("./pages/Testimonials/index.jsx"));
 
-const SpecialisedServicesPage = lazy(() =>
-  import("./pages/SpecialisedServices/index.jsx"),
+const SpecialisedServicesPage = lazy(
+  () => import("./pages/SpecialisedServices/index.jsx"),
 );
 
 const EducationPage = lazy(() => import("./pages/Education/index.jsx"));
 
-const LeisureSportsTourismPage = lazy(() =>
-  import("./pages/LeisureSportsTourism/index.jsx"),
+const LeisureSportsTourismPage = lazy(
+  () => import("./pages/LeisureSportsTourism/index.jsx"),
 );
 
 const ModalitiesPage = lazy(() => import("./pages/Modalities/index.jsx"));
 
-const InfrastructuresPage = lazy(() =>
-  import("./pages/Infrastructures/index.jsx"),
+const InfrastructuresPage = lazy(
+  () => import("./pages/Infrastructures/index.jsx"),
 );
 
 const LogisticsPage = lazy(() => import("./pages/Logistics/index.jsx"));
 
-const ConsultancyPage = lazy(() =>
-  import("./pages/Consultancy/index.jsx"),
-);
+const ConsultancyPage = lazy(() => import("./pages/Consultancy/index.jsx"));
 
 const ContactsPage = lazy(() => import("./pages/Contacts/index.jsx"));
 
 export const sportsRoutes = [
-  <Route
-    key="sports"
-    path="/sunlive-group/sports"
-    element={<SportsShell />}
-  >
+  <Route key="sports" path="/sunlive-group/sports" element={<SportsShell />}>
     <Route index element={<HomePageSports />} />
 
     <Route path="estagios-desportivos" element={<TrainingCampsPage />} />
@@ -78,18 +68,13 @@ export const sportsRoutes = [
     <Route
       path="training-camps"
       element={
-        <Navigate
-          to="/sunlive-group/sports/estagios-desportivos"
-          replace
-        />
+        <Navigate to="/sunlive-group/sports/estagios-desportivos" replace />
       }
     />
 
     <Route
       path="academies"
-      element={
-        <Navigate to="/sunlive-group/sports/academias" replace />
-      }
+      element={<Navigate to="/sunlive-group/sports/academias" replace />}
     />
 
     <Route
@@ -99,49 +84,29 @@ export const sportsRoutes = [
 
     <Route
       path="sunlive-athletes"
-      element={
-        <Navigate
-          to="/sunlive-group/sports/atletas-sunlive"
-          replace
-        />
-      }
+      element={<Navigate to="/sunlive-group/sports/atletas-sunlive" replace />}
     />
 
     <Route
       path="testimonials"
-      element={
-        <Navigate
-          to="/sunlive-group/sports/testemunhos"
-          replace
-        />
-      }
+      element={<Navigate to="/sunlive-group/sports/testemunhos" replace />}
     />
 
     <Route
       path="depoimentos"
-      element={
-        <Navigate
-          to="/sunlive-group/sports/testemunhos"
-          replace
-        />
-      }
+      element={<Navigate to="/sunlive-group/sports/testemunhos" replace />}
     />
 
     <Route
       path="specialised-services"
       element={
-        <Navigate
-          to="/sunlive-group/sports/servicos-especializados"
-          replace
-        />
+        <Navigate to="/sunlive-group/sports/servicos-especializados" replace />
       }
     />
 
     <Route
       path="education"
-      element={
-        <Navigate to="/sunlive-group/sports/educacao" replace />
-      }
+      element={<Navigate to="/sunlive-group/sports/educacao" replace />}
     />
 
     <Route
@@ -166,68 +131,34 @@ export const sportsRoutes = [
 
     <Route
       path="modalities"
-      element={
-        <Navigate
-          to="/sunlive-group/sports/modalidades"
-          replace
-        />
-      }
+      element={<Navigate to="/sunlive-group/sports/modalidades" replace />}
     />
 
     <Route
       path="sports-modalities"
-      element={
-        <Navigate
-          to="/sunlive-group/sports/modalidades"
-          replace
-        />
-      }
+      element={<Navigate to="/sunlive-group/sports/modalidades" replace />}
     />
 
     <Route
       path="infrastructures"
-      element={
-        <Navigate
-          to="/sunlive-group/sports/infraestruturas"
-          replace
-        />
-      }
+      element={<Navigate to="/sunlive-group/sports/infraestruturas" replace />}
     />
 
     <Route
       path="logistics"
-      element={
-        <Navigate
-          to="/sunlive-group/sports/logistica"
-          replace
-        />
-      }
+      element={<Navigate to="/sunlive-group/sports/logistica" replace />}
     />
 
     <Route
       path="consultancy"
-      element={
-        <Navigate
-          to="/sunlive-group/sports/consultoria"
-          replace
-        />
-      }
+      element={<Navigate to="/sunlive-group/sports/consultoria" replace />}
     />
 
     <Route
       path="contacts"
-      element={
-        <Navigate
-          to="/sunlive-group/sports/contactos"
-          replace
-        />
-      }
+      element={<Navigate to="/sunlive-group/sports/contactos" replace />}
     />
 
-    <Route
-      path="*"
-      element={<Navigate to="/sunlive-group/sports" replace />}
-    />
+    <Route path="*" element={<Navigate to="/sunlive-group/sports" replace />} />
   </Route>,
 ];
-
