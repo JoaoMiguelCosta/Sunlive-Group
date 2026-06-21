562bc16d36e028b371097658b1ef952c42f17d86
JoaoMiguelCosta <joaoxxmiguel@hotmail.com>
2026-06-22 00:25:09 +0100

    perf(sports): isolate academies configuration

--- src/brands/sports/config/pages/academies.js
+++ src/brands/sports/config/pages/academies.js
@@ -1,12 +1,5 @@
-import { hero, academiesCards, cta } from "../sections/academies/index.js";
-
-const academies = {
-  id: "academies",
-  sections: {
-    hero,
-    academiesCards,
-    cta,
-  },
-};
-
+import hero from "../sections/academies/hero.js";
+import academiesCards from "../sections/academies/academiesCards.js";
+import cta from "../sections/academies/cta.js";
+const academies = { id: "academies", sections: { hero, academiesCards, cta } };
 export default academies;
