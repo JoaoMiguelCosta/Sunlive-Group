d613f0b0de955b9e6bf75fecbeb92a3e83f2c377
JoaoMiguelCosta <joaoxxmiguel@hotmail.com>
2026-06-22 17:06:02 +0100

    refactor(hotel): remove obsolete config barrels

--- src/brands/hotel/config/pages/index.js
+++
@@ -1,8 +0,0 @@
-export { default as home } from "./home.js";
-export { default as about } from "./about.js";
-export { default as accommodation } from "./accommodation.js";
-export { default as dining } from "./dining.js";
-export { default as facilities } from "./facilities.js";
-export { default as events } from "./events.js";
-export { default as information } from "./information.js";
-export { default as sustainability } from "./sustainability.js";
--- src/brands/hotel/config/sections/index.js
+++
@@ -1,8 +0,0 @@
-export * as home from "./home/index.js";
-export * as about from "./about/index.js";
-export * as accommodation from "./accommodation/index.js";
-export * as dining from "./dining/index.js";
-export * as facilities from "./facilities/index.js";
-export * as events from "./events/index.js";
-export * as information from "./information/index.js";
-export * as sustainability from "./sustainability/index.js";
