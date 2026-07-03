424b84ca04948d1e0d62c8c9721bd26611c7a98b
JoaoMiguelCosta <joaoxxmiguel@hotmail.com>
2026-07-03 21:05:13 +0100

    refactor(group): centralize partner category data
    PARTNER_CATEGORIES agora é a única fonte das 5 categorias de
    parceiros (label/hash/items), substituindo a lista com 6 entradas e
    o placeholder desativado "Outros Parceiros". PARTNER_LINKS deriva
    dela para o footer, e o mesmo array alimentará o novo seletor da
    homepage e o submenu da UtilityBar.

--- src/brands/group/config/sections/home/partners.js
+++ src/brands/group/config/sections/home/partners.js
@@ -1,11 +1,11 @@
-import { PARTNERS_TITLE, PARTNER_LINKS } from "../../core/partners.js";
+import { PARTNERS_TITLE, PARTNER_CATEGORIES } from "../../core/partners.js";
 
 export const partners = {
   id: "partners",
   headline: {
     title: PARTNERS_TITLE,
   },
-  categories: PARTNER_LINKS,
+  categories: PARTNER_CATEGORIES,
 };
 
 export default partners;
